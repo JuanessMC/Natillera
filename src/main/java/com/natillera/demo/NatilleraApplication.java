068958c24b9394c7c2cb83b9f7743e97c3519ece
Juanes <juanluisonia1507@gmail.com>
2024-05-11 21:36:32 -0500

    Commit inicial

---
+++ src/main/java/com/natillera/demo/NatilleraApplication.java
@@ -0,0 +1,13 @@
+package com.natillera.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatilleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatilleraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natillera/demo/NatilleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natillera.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatilleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
